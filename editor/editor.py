6f575071408f0023ade20997fca37c5d423dd820
xpenalosa <xavier.penalosa.esteller@gmail.com>
2019-09-30 19:33:49 +0200

    Self-sufficient editor can now edit itself

--- editor/editor.py
+++ editor/editor.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from pygments.lexers import get_lexer_for_filename
 from subprocess import Popen
 import sys
 
@@ -9,28 +10,30 @@ class EditorWidget(Widget):
     def __init__(self):
         super(EditorWidget, self).__init__()
         self.running_process = None
+        self.open_files = []
+        self.current_file_id = -1
 
     def __del__(self):
         if self.running_process:
             self.running_process.kill()
 
-    @staticmethod
-    def save(file_selector, code_input):
-        # TODO: Add output file selection
-        out_file = file_selector.path + 'outfile.py'
+    def save(self, file_selector, code_input):
+        out_file = self.open_files[self.current_file_id]
         with open(out_file, 'w') as f:
             f.write(code_input.text)
         return out_file
 
-    @staticmethod
-    def open(file_selector, code_input):
+    def open(self, file_selector, code_input):
         if file_selector.selection:
             input_file = file_selector.selection[0]
+            self.open_files.append(input_file)
+            self.current_file_id = len(self.open_files) - 1
             with open(input_file, 'r') as f:
                 code_input.text = f.read()
+                code_input.lexer = get_lexer_for_filename(input_file)
 
-    def run_code(self, code_input):
-        out_file = EditorWidget.save(code_input)
+    def run_code(self, file_selector, code_input):
+        out_file = self.save(file_selector, code_input)
         self.running_process = Popen(["python", out_file])
         while self.running_process.poll() is None:
             self.running_process.wait(0.5)
